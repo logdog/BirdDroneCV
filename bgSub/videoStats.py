9754aab690417b42a6fe9d32acd14908194553f3
Logan Dihel <logan.dihel@gmail.com>
2023-01-17 20:37:38 -0800

    Program to print out video statistics

---
+++ bgSub/videoStats.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+
+video_path = r'E:\research\birdDroneSystem\droneVideos\flight1.mp4'
+
+def main():
+
+    # load the video
+    capture = cv.VideoCapture(video_path)
+    if not capture.isOpened():
+        print('Unable to open: ' + str(capture))
+        exit(0)
+
+    print('Video Path: ', video_path)
+
+    # determine how many frames exist
+    # and frame size
+    numFrames = 0
+    while True:
+        _, frame = capture.read()
+        if frame is None:
+            break
+        numFrames += 1
+        
+        if numFrames == 1:
+            print('Width:  ', frame.shape[1], 'px')
+            print('Height: ', frame.shape[0], 'px')
+        print('Num Frames: ', numFrames,end='\r')
+    capture.release()
+    print()
+    print(f'Runtime: {int(numFrames/(30*60)):02d}:{int((numFrames/30)%60):02d} (mm:ss)')
+    print('Done!')
+
+if __name__ == '__main__':
+    main()
